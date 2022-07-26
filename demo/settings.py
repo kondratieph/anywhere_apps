692ddf52ad527035fa41bdb0ab331ddf639369ac
slava kondratieph <kondratieph@gmail.com>
2022-07-26 18:51:21 +0300

    settings.py updated

--- demo/settings.py
+++ demo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*@d1=&cpcpeh05ncn012*wg^dbft6+uf=_i30n=(^d_ij7*757'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['tieph.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'tieph.pythonanywhere.com']
 
 
 # Application definition
